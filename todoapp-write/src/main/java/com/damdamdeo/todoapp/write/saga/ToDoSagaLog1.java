960a5287242e16309972bc45e7bc0dcfbb05224d
Damien <damien.clementdhuart@gmail.com>
2018-11-04 10:44:15 +0100

    Test multiple saga ok :)

---
+++ todoapp-write/src/main/java/com/damdamdeo/todoapp/write/saga/ToDoSagaLog1.java
@@ -0,0 +1,38 @@
+package com.damdamdeo.todoapp.write.saga;
+
+import java.lang.invoke.MethodHandles;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.axonframework.eventhandling.saga.EndSaga;
+import org.axonframework.eventhandling.saga.SagaEventHandler;
+import org.axonframework.eventhandling.saga.StartSaga;
+
+import com.damdamdeo.todoapp.api.event.ToDoItemCompletedEvent;
+import com.damdamdeo.todoapp.api.event.ToDoItemCreatedEvent;
+import com.damdamdeo.todoapp.api.event.ToDoItemDeadlineExpiredEvent;
+
+public class ToDoSagaLog1 {
+
+	private static final Logger logger = Logger.getLogger(
+			MethodHandles.lookup().lookupClass().getName());
+
+	@StartSaga
+	@SagaEventHandler(associationProperty = "todoId")
+	public void onToDoItemCreated(final ToDoItemCreatedEvent event) {
+		logger.log(Level.INFO, "SagaEventHandling: {0}.", event);
+	}
+
+	@EndSaga
+	@SagaEventHandler(associationProperty = "todoId")
+	public void onDeadlineExpired(final ToDoItemDeadlineExpiredEvent event) {
+		logger.log(Level.INFO, "SagaEventHandling: {0}.", event);
+	}
+
+	@EndSaga
+	@SagaEventHandler(associationProperty = "todoId")
+	public void onToDoItemCompleted(final ToDoItemCompletedEvent event) {
+		logger.log(Level.INFO, "SagaEventHandling: {0}.", event);
+	}
+
+}
